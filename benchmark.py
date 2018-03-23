a71e86a0f74323567ed0c94bfd3ff5ba258808ca
Parimarjan Negi <pnegi@stanford.edu>
2018-03-22 21:52:06 -0700

    runs some stuff

--- benchmark.py
+++ benchmark.py
@@ -7,14 +7,43 @@ import matplotlib
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import json
-NUM_TRIES = 10
+NUM_TRIES = 1
 import math
 
 parser = argparse.ArgumentParser()
-template = 'python main.py -n 100 -p {p}'
+parser.add_argument("-n", type=int, required=True, default=100)
+args = parser.parse_args()
+
+import math
+
+def nCr(n,r):
+    f = math.factorial
+    return f(n) / f(r) / f(n-r)
+
 
 def run_hamiltonian(p):
-    cmd = template.format(p = p)
+    template = 'python main.py -n {n} -p {p} -ham 1'
+    cmd = template.format(n = args.n, p = p)
+    cmd = cmd.split()
+    success = 0.0
+    for i in range(NUM_TRIES):
+        process = sp.Popen(cmd, stdout=sp.PIPE)
+        # process.wait()
+        stdout = process.communicate()[0]
+        stdout = stdout.split('\n')
+        if stdout[0] == "True":
+            success += 1
+
+        # if NUM_TRIES == 20 and success == 0.00:
+            # break
+    
+    print "Ham: {}: {}".format(p, success / float(NUM_TRIES))
+    return (success / float(NUM_TRIES))
+
+def run_connected(p):
+    template = 'python main.py -n {n} -p {p} -connected 1'
+    cmd = template.format(n = args.n, p = p)
+    # print(cmd)
     cmd = cmd.split()
     success = 0.0
     for i in range(NUM_TRIES):
@@ -28,12 +57,38 @@ def run_hamiltonian(p):
         if NUM_TRIES == 20 and success == 0.00:
             break
     
-    print "{}: {}".format(p, success / float(NUM_TRIES))
+    print "Connected: {}: {}".format(p, success / float(NUM_TRIES))
+    return (success / float(NUM_TRIES))
 
+def prob_connected(n, p):
+    summation = 0.00
+    for i in range(1,n+1,1):
+        summation += nCr(n, i) * (1-p)**(i*(n-1))
+        # summation += nCr(n, i) * (1-p)**((n-1))
+    
+    return 1 - summation
 
-for i in range(0, 20, 1):
-    p = 0.01 * i/2
-    exp = math.pow(p, 100) * math.factorial(100-1)
-    print("exp: ", exp)
-    run_hamiltonian(p)
+for n in range(10, 101, 10):
+    print("n = ", n)
+    args.n = n
+    critical = float(math.e) / args.n
+    print("critical: ", critical)
+    print("critical 2: ", critical*2)
+
+    # start_val = critical / 2
+    start_val = critical 
+    hamiltonians = []
+    connecteds = []
+
+    for i in range(0, 10, 1):
+        p = start_val * i 
+        print("prob connected: ", prob_connected(n, p))
+        # h = run_hamiltonian(p)
+        c = run_connected(p)
+
+        # if (c > 0.00): 
+            # connecteds.append(c)
+
+        # if (h > 0.0 and c > 0.0): 
+            # break
 
--- main.py
+++ main.py
@@ -6,12 +6,13 @@ def create_complete_graph(n, p):
     g = Graph()
     for i in range(n):
         g.add_vertex(str(i))
-    # assert g.__graph_dict['0'].vertex_degree == 0, 'test'
+    # assert g.__graph['0'].vertex_degree == 0, 'test'
 
     # now add edges of complete graph with probability p
     for i in range(n):
         for j in range(n):
-            if (i == j): continue
+            if (i >= j): continue
+            # if (i == j): continue
             if random.random() <= p:
                 # add an edge. Otherwise ignore.
                 edges = (str(i), str(j))
@@ -32,23 +33,27 @@ if __name__ == "__main__":
     # parser.add_argument("-kgraph", type=int, required=False,
                         # default=1, help="run k-graph simulation") 
     parser.add_argument("-hamiltonian", "-ham",  type=int, required=False,
-                        default=1, help="run hamiltonian simulation") 
+                        default=0, help="run hamiltonian simulation") 
     parser.add_argument("-chromatic", "-chrom",  type=int, required=False,
-                        default=1, help="run hamiltonian simulation") 
+                        default=0, help="run hamiltonian simulation") 
+    parser.add_argument("-connected", "-connected",  type=int, required=False,
+                        default=0, help="run hamiltonian simulation") 
 
     args = parser.parse_args()
     g = create_complete_graph(args.n, args.p)
     
     # print("isolated: ", g.find_isolated_vertices())
-    # print("delta: ", g.delta()) 
-    # print("Delta: ", g.delta()) 
-    # print("connected: ", g.is_connected())
+    if args.connected:
+        # print("delta: ", g.delta()) 
+        # print("Delta: ", g.Delta()) 
+        # print("connected: ", g.is_connected())
+        print(g.is_connected())
     # print("num edges: ", len(g.edges()))
     # print("diameter: ", g.diameter())
     # print(g.find_cycle("0"))
     
     if args.hamiltonian:
-        if (g.find_cycle("0", cycle_len=len(g._graph_dict))) is None:
+        if (g.find_cycle("0", cycle_len=len(g._graph))) is None:
             print("False")
         else:
             print("True")
